cf79e83f2b92a47a2807fa30ec8aa889929e2de5
rrodriguesrr <rodrirub@outlook.com>
2026-02-17 00:09:05 +0000

    Deploying to gh-pages from @ rrodriguesrr/rrodriguesrr.github.io@4c57f05465346ba26e760b5dc9ae44631dec762e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,7 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%6F%64%72%69%72%75%62@%6F%75%74%6C%6F%6F%6B.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
